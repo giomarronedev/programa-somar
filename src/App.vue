9fc1417eaf1fc503fb8dfa0516b56abd6b58b87a
Giovanne Marrone <giovannemarrone@gmail.com>
2023-04-04 00:08:44 -0300

    began landing

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <LandingPage />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
-</script>
-
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  --brand-blue: #0C4D8A;
+  --brand-oragne: #F38D04;
+}
+
+html {
+  font-size: 62.5%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+h1 {
+  font-size: 3.6rem;
+}
+
+.wrapper {
+  width: 100%;
+  max-width: 112rem;
+  margin: 0 auto;
 }
 </style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LandingPage from "./Pages/Landing.vue";
+
+export default defineComponent({
+    name: "App",
+    components: { LandingPage }
+});
+</script>
---
+++ src/Pages/Landing.vue
@@ -0,0 +1,34 @@
+<template>
+  <section id="welcome">
+    <div class="white-bg-mask">
+      <div class="wrapper">
+        <div class="logo-area">
+            <img src="../assets/somar-logo.svg" alt="">
+        </div>
+        <h1>teste</h1>
+      </div>
+    </div>
+  </section>
+</template>
+<style>
+section {
+    background-image: url("../assets/landing-bg.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;
+}
+.logo-area {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    border: 3px solid var(--brand-blue);
+    border-top: 0;
+    padding: 3rem;
+}
+</style>
+<script lang="ts">
+export default {
+  name: "LandingPage",
+};
+</script>
